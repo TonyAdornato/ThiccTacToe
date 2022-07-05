e904ba0a659baf0bffd131e08088619aa6865ec6
TonyAdornato <97354785+TonyAdornato@users.noreply.github.com>
2022-07-05 17:46:23 -0400

    Update ThiccTacToe_v2.py

--- ThiccTacToe_v2.py
+++ ThiccTacToe_v2.py
@@ -4,6 +4,7 @@ It allows the player to play a game of tic tac to on a grid that is 3x3 or large
 Created by Tony Adornato
 """
 
+# Get the desired board size for the game
 def get_board_size():
     print("Welcome to Thicc Tac Toe.")
     print("The game that lets you play tic tac toe on any size grid.")
@@ -11,11 +12,11 @@ def get_board_size():
     while True: 
         try:
             grid_size = int(input("Enter a grid size: "))
-            if grid_size <= 0:
+            if grid_size <= 0:          # Grid size is 0 or less. 
                 print("Yes. You're very clever.")
-            elif grid_size <= 2:
+            elif grid_size <= 2:        # Grid is not big enough for a meaningful game
                 print("That grid is not very thi" + ("c" * grid_size) + ".")
-            elif grid_size == 3:
+            elif grid_size == 3:        # Standard size board
                 print("Ah! An old fashioned.")
                 return grid_size
             else:
@@ -24,27 +25,32 @@ def get_board_size():
         except ValueError:
             print("Please enter an integer number.")  
 
+# builds the board for the game. 
+# generates an array with layout [[x , y , value], ...]
 def generate_board(size):
     new_board = []
     for x in range(1, size+1):
-        for y in range(1, (size+1)):
+        for y in range(1, (size+1)): 
             new_board.append([x, y , " "])
     return new_board
 
 
+# displays the board in the terminal with the 
 def draw_board(size, board):
-    numbers = [str("%3d" % num) for num in range(1, size+1)]
-    print("    " + " ".join(numbers))
+    numbers = [str("%3d" % num) for num in range(1, size+1)]    # generate the labels for the top based on the size of the board
+    print("    " + " ".join(numbers))       # print a row at the top with all of the indexes for the y position.
 
-    res = [(" " + entry[2]+ " ") for entry in board]
+    res = [(" " + entry[2]+ " ") for entry in board]        # create a list of all of the rows in the board
     start = 0
     end = size
-    for x in range(1, size+1):
-        print(str("%3d" % x) + ": " + "|".join(res[start:end]))
+    for x in range(1, size+1):      # print each row of the board
+        print(str("%3d" % x) + ": " + "|".join(res[start:end]))         # print each value in the row, with a | separating each
         start += size
         end +=size
 
 
+# Takes the result for user input of a square
+# returns the result for the index of the array with the correct x and y values
 def get_square(board, indices, values):
     #want pos of item having [row, column] in positions 0 and 1
     for pos, k in enumerate(board):
@@ -57,6 +63,10 @@ def get_square(board, indices, values):
             return pos
     raise ValueError("board.index(x): x not in list")
 
+    
+# User inputs a square to play. 
+# Will not let a player enter a square that has already been played
+# writes the correct symbol to the array if the rquare is valid
 def get_move(size, board, symbol):
     print(str(symbol) + " move")
     while True: 
@@ -79,6 +89,7 @@ def get_move(size, board, symbol):
             print("Please enter integer numbers.")
 
 
+            # Checks if either player has won the game, or if it is a tie
 def check_win_state(size, board):
 
     #check rows and columns
@@ -113,12 +124,15 @@ def check_win_state(size, board):
 
     #No one has won. Continue game
     return False
-    
+
+# Main game loop
 def main():
     while True:
+        # initialize the game
         board_size = get_board_size()
         board = generate_board(board_size)
         draw_board(board_size, board)
+        
         while True:
             #player 1 move
             get_move(board_size, board, "X")
